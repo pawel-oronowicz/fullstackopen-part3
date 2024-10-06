16f48734f4105f85428d2ad83d536f555ccfa44d
Paweloronowicz <oronowicz@gmail.com>
2024-10-06 23:01:40 +0200

    Phonebook - saving to DB

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
+const url = process.env.MONGODB_URI;
 
 morgan.token('data', function (req, res) {
   return JSON.stringify(req.body) 
@@ -13,26 +17,26 @@ morgan.token('data', function (req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
-  { 
-    id: "1",
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: "2",
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: "3",
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: "4",
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
+  // { 
+  //   id: "1",
+  //   name: "Arto Hellas", 
+  //   number: "040-123456"
+  // },
+  // { 
+  //   id: "2",
+  //   name: "Ada Lovelace", 
+  //   number: "39-44-5323523"
+  // },
+  // { 
+  //   id: "3",
+  //   name: "Dan Abramov", 
+  //   number: "12-43-234345"
+  // },
+  // { 
+  //   id: "4",
+  //   name: "Mary Poppendieck", 
+  //   number: "39-23-6423122"
+  // }
 ]
 
 const generateId = () => {
@@ -54,6 +58,10 @@ const validateName = (enteredName) => {
   return false
 }
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -65,18 +73,15 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-  if(person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -101,18 +106,20 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+    // id: generateId(),
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
